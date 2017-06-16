4d916636dcbe9a178e7e80368ae9ee1ba0e1ca60
Steve Spitz <steve@spitz.com>
2017-06-16 12:59:14 -0700

    Add no size warning, add toppings list, add warning print function, add pizzaOrder object type and listners

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
+var toppingsArray = []
+var totalPrice
+errorReadoutSize = function() {
+  $("#warningPlace").text("Please be sure to select a size for your pizza!")
+}
+
 $(document).ready(function(){
   $("form#radioForm").submit(function(event) {
     event.preventDefault();
-    
+
+    function pizzaorder(toppings[], size){
+      this.toppings = toppings;
+      this.size = size;
+    };
+    pizzaorder.totalCalc() = function() {
+      if (size == "Small") {
+      totalPrice = (toppings.length * 2) + 4
+    } else if (size == "Medium") {
+      totalPrice = (toppings.length * 2) + 6
+    } else if (size == "Large") {
+      totalPrice = (toppings.length * 2) + 8
+    } else {
+      errorReadoutSize()
+    };
+      
+    };
+
+    var userOrder = new pizzaOrder(toppingsArray, pizzaSizeArg);
+
     $("button#currentValueButton").click(function(){
-      costModifier = $("#pizzaSize input:radio:checked").val();
+      pizzaSizeArg = $("#pizzaSize input:radio:checked").val();
       console.log(costModifier);
+      $("#toppingsList input:checkbox:checked").val().push(toppingsArray);
+      console.log(toppingsArray);
     });
   });
 });
