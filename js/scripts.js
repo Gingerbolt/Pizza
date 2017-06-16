5053a0ff9ee3db78e08dd96646ee39505bf995d2
Steve Spitz <steve@spitz.com>
2017-06-16 16:30:59 -0700

    Add functioning conditionals. Add front end warnings regarding invalid input;

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,25 @@
-var size;
 function pizzaOrder(toppings, size){
   this.toppings = toppings;
   this.size = size;
 };
-
 pizzaOrder.prototype.totalCalc = function() {
-  if (size == "Small") {
-  return (toppings.length * 2) + 4
-  console.log("small triggered")
-} else if (size == "Medium") {
-  return (toppings.length * 2) + 6
-  console.log("medium triggered")
-} else if (size == "Large") {
-  return (toppings.length * 2) + 8
-  console.log("large triggered")
+  if (this.size == 1) {
+  totalPrice = "$" + ((this.toppings.length * 2) + 4) + ".00";
+} else if (this.size == 2) {
+  totalPrice = "$" + ((this.toppings.length * 2) + 6) + ".00";
+} else if (this.size == 3) {
+  totalPrice = "$" + ((this.toppings.length * 2) + 8) + ".00";
 } else {
-  errorReadoutSize()
-};
+  totalPrice = "Invalid Order"
+  errorReadoutSize();
+}
+priceReadout();
 };
 errorReadoutSize = function() {
   $("#warningPlace").text("Please be sure to select a size for your pizza!")
 }
-priceReadout = function(totalPrice) {
-  $("#priceOutput").text("$" + totalPrice + ".00")
+priceReadout = function() {
+  $("#priceOutput").text(totalPrice)
   $("#priceOutput").show()
 }
 
@@ -35,13 +32,11 @@ $(document).ready(function(){
   $("button#currentValueButton").click(function(event){
     event.preventDefault();
     pizzaSizeArg = $("input[type='radio'][name='sizeRadio']:checked").val();
-    pizzaSizeOrder = pizzaSizeArg.toString();
     var toppingsArray = [];
     $("input[type='checkbox']:checked").each(function (){
       toppingsArray.push($(this).val());
     });
-    var userOrder = new pizzaOrder(toppingsArray, pizzaSizeOrder);
-    console.log();
-    priceReadout();
+    var userOrder = new pizzaOrder(toppingsArray, pizzaSizeArg);
+    userOrder.totalCalc()
   });
 });
