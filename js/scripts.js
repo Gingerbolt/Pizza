73a799f0eaad41bb22f025f43cd5f017caf972ae
Steve Spitz <steve@spitz.com>
2017-06-16 15:50:40 -0700

    Gave up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,27 @@
-var toppingsArray = [];
 var size;
 function pizzaOrder(toppings, size){
-  this.toppings = [];
+  this.toppings = toppings;
   this.size = size;
 };
-totalPriceSmall = function() {
-var totalPrice = (toppings.length * 2) + 4
-}
-totalPriceMedium = function() {
-var totalPrice = (toppings.length * 2) + 6
-}
-totalPriceLarge = function() {
-var totalPrice = (toppings.length * 2) + 8
-}
-pizzaOrder.prototype.totalCalc = function(toppings, size) {
+
+pizzaOrder.prototype.totalCalc = function() {
   if (size == "Small") {
-  totalPriceSmall()
-  priceReadout()
+  return (toppings.length * 2) + 4
   console.log("small triggered")
 } else if (size == "Medium") {
-  totalPriceMedium()
-  priceReadout()
+  return (toppings.length * 2) + 6
   console.log("medium triggered")
 } else if (size == "Large") {
-  totalPriceLarge()
-  priceReadout()
+  return (toppings.length * 2) + 8
   console.log("large triggered")
 } else {
   errorReadoutSize()
 };
-
 };
 errorReadoutSize = function() {
   $("#warningPlace").text("Please be sure to select a size for your pizza!")
 }
-priceReadout = function() {
-  totalPrice = totalPrice.toString()
+priceReadout = function(totalPrice) {
   $("#priceOutput").text("$" + totalPrice + ".00")
   $("#priceOutput").show()
 }
@@ -49,11 +35,13 @@ $(document).ready(function(){
   $("button#currentValueButton").click(function(event){
     event.preventDefault();
     pizzaSizeArg = $("input[type='radio'][name='sizeRadio']:checked").val();
-    $("#toppingsList input:checked").each(function (){
+    pizzaSizeOrder = pizzaSizeArg.toString();
+    var toppingsArray = [];
+    $("input[type='checkbox']:checked").each(function (){
       toppingsArray.push($(this).val());
     });
-    var userOrder = new pizzaOrder(toppingsArray, pizzaSizeArg);
-    userOrder.totalCalc();
-    console.log(pizzaSizeArg);
+    var userOrder = new pizzaOrder(toppingsArray, pizzaSizeOrder);
+    console.log();
+    priceReadout();
   });
 });
